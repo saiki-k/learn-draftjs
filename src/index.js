669eb6a62efa954ff9afc2984e15d80bb8a8ab3c
Sai Kishore Komanduri <saikishore.bits@gmail.com>
2017-08-08 22:18:40 +0530

    Update README

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import registerServiceWorker from './sw';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
