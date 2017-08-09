11b10278c49752394c6c45b1a79194da625af283
Sai Kishore Komanduri <saikishore.bits@gmail.com>
2017-08-09 13:03:41 +0530

    Refactor code into components

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './sw';
 
 ReactDOM.render(<App />, document.getElementById('root'));
